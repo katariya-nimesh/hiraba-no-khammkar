d4f7df326bb2792b9982fae14bef54f74cc773cf
Nimesh Katariya <nimesh.katariya23@gmail.com>
2026-02-15 00:26:01 +0530

    Add Home Page Design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
